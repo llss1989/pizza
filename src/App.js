a532dd53f88e878f0341aea1e441b586eae501a2
llss1989 <argenkasymov2@gmail.com>
2023-02-05 14:14:16 +0300

    added prettier and eslint, add interactivity in catefories

--- src/App.js
+++ src/App.js
@@ -1,59 +1,68 @@
-import './scss/app.scss';
-import Header from './components/Header/Header';
-import Categories from './components/Categories/Categories';
-import PizzaBlock from './components/PizzaBlock/PizzaBlock';
-import Sort from './components/Sort/Sort';
+import './scss/app.scss'
+import Header from './components/Header/Header'
+import Categories from './components/Categories/Categories'
+import PizzaBlock from './components/PizzaBlock/PizzaBlock'
+import Sort from './components/Sort/Sort'
 
 function App() {
-  const pizzas = [ {
-    image: "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", 
-    name: 'Чизбургер-пицца', 
-    types: [0,1], 
-    sizes: [26, 30, 40] 
-  },
-  {
-    image: "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", 
-    name: 'Чизбургер-пицца', 
-    types: [0,1], 
-    sizes: [26, 30, 40] 
-  },
-  {
-    image: "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", 
-    name: 'Чизбургер-пицца', 
-    types: [0,1], 
-    sizes: [26, 30, 40] 
-  },
-  {
-    image: "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", 
-    name: 'Чизбургер-пицца', 
-    types: [0,1], 
-    sizes: [26, 30, 40] 
-  },
-  {
-    image: "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", 
-    name: 'Чизбургер-пицца', 
-    types: [0,1], 
-    sizes: [26, 30, 40] 
-  }
-  ]
-  return (
-          <div className="wrapper">
-            <Header/>
-      <div className="content">
-        <div className="container">
-          <div className="content__top">
-              <Categories/>
-              <Sort></Sort>
-          </div>
-          
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {pizzas.map((pizza, index) => <PizzaBlock  key={index} name={pizza.name} image={pizza.image} types={pizza.types} sizes={pizza.sizes} ></PizzaBlock>)}
-          </div>
+    const pizzas = [
+        {
+            image: 'https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg',
+            name: 'Чизбургер-пицца',
+            types: [0, 1],
+            sizes: [26, 30, 40],
+        },
+        {
+            image: 'https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg',
+            name: 'Чизбургер-пицца',
+            types: [0, 1],
+            sizes: [26, 30, 40],
+        },
+        {
+            image: 'https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg',
+            name: 'Чизбургер-пицца',
+            types: [0, 1],
+            sizes: [26, 30, 40],
+        },
+        {
+            image: 'https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg',
+            name: 'Чизбургер-пицца',
+            types: [0, 1],
+            sizes: [26, 30, 40],
+        },
+        {
+            image: 'https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg',
+            name: 'Чизбургер-пицца',
+            types: [0, 1],
+            sizes: [26, 30, 40],
+        },
+    ]
+    return (
+        <div className="wrapper">
+            <Header />
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categories />
+                        <Sort></Sort>
+                    </div>
+
+                    <h2 className="content__title">Все пиццы</h2>
+                    <div className="content__items">
+                        {pizzas.map((pizza, index) => (
+                            <PizzaBlock
+                                key={index}
+                                name={pizza.name}
+                                image={pizza.image}
+                                types={pizza.types}
+                                sizes={pizza.sizes}
+                            ></PizzaBlock>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    )
 }
 
-export default App;
+export default App
--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,16 +1,36 @@
-function Categories () {
-    return (
-      <div className="categories">
-      <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
-      </ul>
-    </div>
-    )
-  }
+import { useState } from 'react'
 
-  export default Categories;
+function Categories() {
+    const [activeCategory, setActiveCategory] = useState(0)
+    const categories = [
+        'Все',
+        'Мясные',
+        'Вегетарианская',
+        'Гриль',
+        'Острые',
+        'Закрытые',
+    ]
+    return (
+        <div className="categories">
+            <ul>
+                {/* <li className="active">Все</li>
+                <li>Мясные</li>
+                <li>Вегетарианская</li>
+                <li>Гриль</li>
+                <li>Острые</li>
+                <li>Закрытые</li> */}
+                {categories.map((category, index) => (
+                    <li
+                        onClick={() => setActiveCategory(index)}
+                        className={activeCategory === index ? 'active' : ''}
+                        key={index}
+                    >
+                        {category}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default Categories
