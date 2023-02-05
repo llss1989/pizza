f403fda91db06c8b1c0759e044722f883bf6f3d4
llss1989 <argenkasymov2@gmail.com>
2023-02-05 12:24:16 +0300

    added html, scss, created components Pizzablock, Sort, HEader, categories

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import './scss/app.scss';
+import Header from './components/Header/Header';
+import Categories from './components/Categories/Categories';
+import PizzaBlock from './components/PizzaBlock/PizzaBlock';
+import Sort from './components/Sort/Sort';
 
 function App() {
+  const pizzas = [ {
+    image: "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", 
+    name: 'Чизбургер-пицца', 
+    types: [0,1], 
+    sizes: [26, 30, 40] 
+  },
+  {
+    image: "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", 
+    name: 'Чизбургер-пицца', 
+    types: [0,1], 
+    sizes: [26, 30, 40] 
+  },
+  {
+    image: "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", 
+    name: 'Чизбургер-пицца', 
+    types: [0,1], 
+    sizes: [26, 30, 40] 
+  },
+  {
+    image: "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", 
+    name: 'Чизбургер-пицца', 
+    types: [0,1], 
+    sizes: [26, 30, 40] 
+  },
+  {
+    image: "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", 
+    name: 'Чизбургер-пицца', 
+    types: [0,1], 
+    sizes: [26, 30, 40] 
+  }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <div className="wrapper">
+            <Header/>
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+              <Categories/>
+              <Sort></Sort>
+          </div>
+          
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {pizzas.map((pizza, index) => <PizzaBlock key={index} name={pizza.name} image={pizza.image} types={pizza.types} sizes={pizza.sizes} ></PizzaBlock>)}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
